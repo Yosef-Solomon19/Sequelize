9e29d74f6410e9727ec3d8e4a32de289a24b87af
Yosef-Solomon19 <ysolomon@terpmail.umd.edu>
2021-03-29 18:39:44 -0400

    V3 Add table layout html Add styling layout css Needs work Js

--- public/scripts.js
+++ public/scripts.js
@@ -1,10 +1,15 @@
 
-
 async function dataHandler() {
-    console.log('window loaded');
-    
+  console.log('window loaded');
+  const request = await fetch('http://localhost:3000/api/dining', {method: 'GET'});
+  const data = await request.json();
+  return data;
 
 }
-
+async function windowActions() {
+  console.log('window loaded');
+  const dataTable = await getData();
+  console.table(dataTable);
+}
 
 window.onload = windowActions;

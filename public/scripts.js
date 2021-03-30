61863a657fac3693591c41e308415d0f7e5bcb4e
Yosef-Solomon19 <ysolomon@terpmail.umd.edu>
2021-03-30 16:51:40 -0400

    V4: adding table in script.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,11 +1,28 @@
+async function getDining() {
+  const output = document.querySelector('#myTable');
+  const Diningrequest = await fetch('/api/dining');
+  // waits until the request completes...
+  console.log('response');
+  const diningData = await Diningrequest.json();
+  
+  diningData.data.forEach((diningHall) => {
+    console.log('diningHall')
+    const appendItem = document.createElement('tr');
+    appendItem.innerHTML = `
+    
+    `;
 
-async function dataHandler() {
-  console.log('window loaded');
-  const request = await fetch('http://localhost:3000/api/dining', {method: 'GET'});
-  const data = await request.json();
-  return data;
 
-}
+
+
+  });
+
+
+
+
+
+
+
 async function windowActions() {
   console.log('window loaded');
   const dataTable = await getData();

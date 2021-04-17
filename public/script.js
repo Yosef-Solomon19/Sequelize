7d62a2aebae81d0726b1180e028f76455515745d
Yosef-Solomon19 <ysolomon@terpmail.umd.edu>
2021-04-16 21:23:05 -0400

    Working on dataChart function

--- public/script.js
+++ public/script.js
@@ -23,8 +23,88 @@ async function windowActions() {
   });
  // Challenge: How to display the randomly selected Meals/Macros on a table. 
   console.table(selectedMeals);
-  console.table(mealData);
+  return selectedMeals;
 }
 
-window.onload = windowActions;
+function mealChart(selectedMeals,macros) {
+  console.table(selectedMeals);
+  const macroName = [];
+  selectedMeals.forEach((meal) =>{
+    const macroNameDict = {y: '', label: ''};
+    macroNameDict.label = meal.meal_name;
+    macroNameDict.y = meal[macros];
+    macroName.push(macroNameDict);
+  });
+  return macroName;
+}
 
+
+window.onload = async function () {
+  let dataChart = await windowActions();
+  const chart = new CanvasJS.Chart('chartContainer', {
+    animationEnabled: true,
+    title: {
+      text: '10 Random Meals Macro Chart'
+    },
+    axisX : {
+
+    },
+    axisY : {
+
+    },
+    toolTip: {
+      shared: true
+    },
+    legend: {
+      cursor: 'pointer',
+      itemclick: toggleDataSeries
+    },
+    data: [{
+      type: 'stackedBar',
+      name: 'calories',
+      showInLegend: true,
+      datapoints: mealChart(dataChart, 'calories')
+    },
+    {
+      type: 'stackedBar',
+      name: 'cholesterol',
+      showInLegend: true,
+      datapoints: mealChart(dataChart, 'cholesterol')
+    },
+    {
+      type: 'stackedBar',
+      name: 'sodium',
+      showInLegend: true,
+      datapoints: mealChart(dataChart, 'sodium')
+    },
+    {
+      type: 'stackedBar',
+      name: 'carbs',
+      showInLegend: true,
+      datapoints: mealChart(dataChart, 'carbs')
+    },
+    {
+      type: 'stackedBar',
+      name: 'protein',
+      showInLegend: true,
+      datapoints: mealChart(dataChart, 'protein')
+    },
+    {
+      type: 'stackedBar',
+      name: 'fat',
+      showInLegend: true,
+      datapoints: mealChart(dataChart, 'fat')
+    }]
+  });
+  chart.render();
+  // console.log(chart);
+  
+  function toggleDataSeries(e) {
+    if (typeof (e.dataSeries.visible) === 'undefined' || e.dataSeries.visible) {
+      e.dataSeries.visible = false;
+    } else {
+      e.dataSeries.visible = true;
+    }
+    chart.render();
+  }
+};
